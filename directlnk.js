33e95cadcac151ffb46b60a3cd1e5b4534e7f4e1
fatinahasma <154037911+fatinahasma@users.noreply.github.com>
2023-12-17 13:32:02 +0700

    Create directlnk.js

---
+++ directlnk.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://careersadorable.com/qd9g689m?key=3333f660b1fa146c02b58584a839c983', '_blank');
+	});
